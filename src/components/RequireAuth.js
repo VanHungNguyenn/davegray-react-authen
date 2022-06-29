b18973da4f61eb0453652d818a6324ca4d3ebed7
VanHungNguyen <vanhungnguyenbku@gmail.com>
2022-06-29 11:24:20 +0700

    .

--- src/components/RequireAuth.js
+++ src/components/RequireAuth.js
@@ -6,9 +6,11 @@ const RequireAuth = ({ allowedRoles }) => {
 	const { auth } = useAuth()
 	const location = useLocation()
 
-	const decoded = auth?.accessToken
+	const decoded = auth?.accessToken ? jwt_decode(auth.accessToken) : undefined
 
-	return auth?.roles?.find((role) => allowedRoles?.includes(role)) ? (
+	const roles = decoded?.UserInfo?.roles || []
+
+	return roles?.find((role) => allowedRoles?.includes(role)) ? (
 		<Outlet />
 	) : auth?.user ? (
 		<Navigate to='/unauthorized' state={{ from: location }} replace />
